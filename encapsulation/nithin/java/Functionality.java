ba0190d5e7d248b4b9f10352938c90d6043b221e
Polkam-Nithin <153354507+Polkam-Nithin@users.noreply.github.com>
2024-05-17 22:25:37 +0530

    Add files via upload

---
+++ encapsulation/nithin/java/Functionality.java
@@ -0,0 +1,21 @@
+package nithin.java;
+
+public class Functionality {
+	private int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String verify()
+	{
+		if(age>=18)
+			return "Eligible for Vote";
+		else
+			return "Not Eligible";
+	}
+
+}
---
+++ encapsulation/nithin/java/MainFunctionality.java
@@ -0,0 +1,11 @@
+package nithin.java;
+
+public class MainFunctionality {
+
+	public static void main(String[] args) {
+		Functionality fun=new Functionality();
+	    fun.setAge(1);
+           System.out.println(fun.verify());
+	}
+
+}
